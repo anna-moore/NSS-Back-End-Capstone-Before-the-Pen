8f56ea241272b819d88c4d0bbd822d843b66649d
anna-moore <ammoore434@gmail.com>
2021-04-30 10:22:27 -0500

    update startup with JWT, created DbUtils, start UserProfileRepo

---
+++ BeforeThePen/BeforeThePen/Repositories/UserProfileRepository.cs
@@ -0,0 +1,71 @@
+﻿using BeforeThePen.Models;
+using Microsoft.Extensions.Configuration;
+using BeforeThePen.Utils;
+using static BeforeThePen.Utils.DbUtlis;
+
+namespace BeforeThePen.Repositories
+{
+    public class UserProfileRepository : BaseRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) { }
+
+        public UserProfile GetByFirebaseUserId(string firebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT up.Id, up.FirebaseId, up.DisplayName, up.FirstName, up.LastName, up.Email, 
+                               up.DateCreated, up.ImageURL
+                          FROM UserProfile up                             
+                         WHERE FirebaseUserId = @FirebaseuserId";
+
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", firebaseUserId);
+
+                    UserProfile userProfile = null;
+
+                    var reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        userProfile = new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirebaseId = DbUtils.GetString(reader, "FirebaseUserId"),
+                            DisplayName =  DbUtils.GetString(reader, "DisplayName"),
+                            FirstName = DbUtils.GetString(reader, "FirstName"),
+                            LastName = DbUtils.GetString(reader, "LastName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+                            ImageURL = DbUtils.GetString(reader, "ImageURL")                           
+                        };
+                    }
+                    reader.Close();
+
+                    return userProfile;
+                }
+            }
+        }
+
+        public void Add(UserProfile userProfile)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO UserProfile (FirebaseUserId, Name, Email, UserTypeId)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@FirebaseUserId, @Name, @Email, @UserTypeId)";
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", userProfile.FirebaseUserId);
+                    DbUtils.AddParameter(cmd, "@Name", userProfile.Name);
+                    DbUtils.AddParameter(cmd, "@Email", userProfile.Email);
+                    DbUtils.AddParameter(cmd, "@UserTypeId", userProfile.UserTypeId);
+
+                    userProfile.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
--- BeforeThePen/BeforeThePen/Startup.cs
+++ BeforeThePen/BeforeThePen/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-//using BeforeThePen.Repositories;
+using BeforeThePen.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
