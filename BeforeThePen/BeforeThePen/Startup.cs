db9346182975e554ed6e13fc1bac7abe83244b10
anna-moore <ammoore434@gmail.com>
2021-04-30 11:45:55 -0500

    client + proxy set up

--- BeforeThePen/BeforeThePen/Startup.cs
+++ BeforeThePen/BeforeThePen/Startup.cs
@@ -29,6 +29,7 @@ namespace BeforeThePen
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IUserProfileRepository, UserProfileRepository>();
 
             var firebaseProjectId = Configuration.GetValue<string>("beforethepen-5616b");
             var googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
