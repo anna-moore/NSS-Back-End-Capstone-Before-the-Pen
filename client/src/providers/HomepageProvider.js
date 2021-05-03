665a6d0c8ede0b09b6ab538300b97576ba1f08a2
anna-moore <ammoore434@gmail.com>
2021-05-03 17:17:58 -0500

    create provider for homepage

---
+++ client/src/providers/HomepageProvider.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, createContext } from 'react';
+import { Spinner } from 'reactstrap';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const HomepageContext = createContext();
+
+export function HomepageProvider(props) {
+    const apiURL = '/api/homepage';
+
+    const [isLoggedIn, setIsLoggedIn] = useState(userProfile != null);
+    const userProfile = sessionStorage.getItem('userProfile');
+    const [homepageResourceLinks, setHomepageResourceLinks] = useState("");
+    const [spotlight, setSpotlight] = useState("")
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            setCurrentUserId(JSON.parse(userProfile).id);
+        }
+    }, [userProfile]);
+
+    const GetSpotlight = () => {
+        return getToken().then((token) =>
+            fetch(`${apiURL}/currentSpotlight`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then((resp) => resp.json())
+                .then(setSpotlight)
+
+        );
+    };
+
+    //how should this be updated to for a list??
+    const getResourceLinks = () => {
+        return getToken().then((token) => {
+            fetch(`${apiURL}/helpfulLinks`, {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then((resp) => resp.json())
+                .then(setHomepageResourceLinks)
+        });
+    };
+
+    return (
+        <HomepageContext.Provider
+            value={{
+                getResourceLinks,
+                GetSpotlight,
+                setSpotlight,
+                setHomepageResourceLinks,
+                spotlight,
+                homepageResourceLinks
+
+            }}
+        ></HomepageContext.Provider>
+    );
+};
