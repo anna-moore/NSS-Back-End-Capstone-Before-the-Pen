f98cf8f6c50bede7d0e8db9ec00fb497ce28692f
anna-moore <ammoore434@gmail.com>
2021-05-05 13:52:55 -0500

    update database to have youtubeEmbedId instead of URL, create function to embed video

--- BeforeThePen/BeforeThePen/Models/Spotlight.cs
+++ BeforeThePen/BeforeThePen/Models/Spotlight.cs
@@ -9,7 +9,7 @@ namespace BeforeThePen.Models
     {
         public int Id {get; set;}
         public string Artist { get; set; }
-        public string VideoURL { get; set; }
+        public string YoutubeEmbedId { get; set; }
         public string ArtistPortfolioURL { get; set; }
         public string ImageURL { get; set; }
         public string About { get; set; }
--- client/src/components/hompage/Spotlight.js
+++ client/src/components/hompage/Spotlight.js
@@ -2,40 +2,83 @@
 //must have nested if clause for the items that are nullable
 
 //list out all of the links
-import React, { useContext, useEffect, useState } from "react"
-import { HomepageContext } from "../../providers/HomepageProvider"
-import HomepageResourceLinksCard from './HomepageResourceLinksCard'
-import { Col, Row, Container } from 'reactstrap';
+import React, { useContext, useEffect, useState } from "react";
+import { HomepageContext } from "../../providers/HomepageProvider";
+import HomepageResourceLinksCard from './HomepageResourceLinksCard';
+import { Col, Row, Container, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+import "../../App.css";
+import YoutubeEmbed from "./SpotlightVideo";
 
-const HelpfulResourceList = () => {
-    const { getSpotlight, setSpotlight } = useContext(HomepageContext);
+export const SpotLightListCard = () => {
+    const { getSpotlight, setSpotlight, spotlight } = useContext(HomepageContext);
 
     useEffect(() => {
         getSpotlight();
     }, []);
 
 
+    //this card will have 
+    //title of current Month 's Spotlight ⛅ 
+    // artist name with a link to their portfolio ⛅ 
+    //an optional video of their layout ⛅ 
+    //or image made with an imageURL their layout⛅ 
+    //the about section blur ⛅ 
+    //style to flex the video or the image to the left and the other content to the right 
+    //adjust the margin 
+
+    {
+        comments.length > 0 ?
+            (comments.map((comment) => {
+                return <CommentCard key={comment.id} comment={comment} commentState={commentState} />
+            })
+            ) : (
+                <span style={{ marginTop: "30px" }}>No comments on this post.</span>
+            )
+    }
     return (
 
         <Container>
-            <Col xs="8" className="container">
-                <div className="row justify-content-center">
-                    <div className="cards-column">
+            <Card className="m-4">
+                <CardBody>
+                    <CardTitle>May's Spotlight</CardTitle>
+                    <CardSubtitle><a href={spotlight.artistPortfolioURL}>{spotlight.name}</a></CardSubtitle>
+                    <CardText>{spotlight.about}</CardText>
+                    {/* this is where I have a ternary with the image or the video 
+                        if spotlight.youtubeEmbedId does not equal null then display the video
+                        if {above} does equal null than display spotlight.imageURL*/}
+                    {
+                        spotlight.youtubeEmbedId != null ?
+                            (<div className="App">
+                                <h1>Youtube Embed</h1>
+                                <YoutubeEmbed embedId={spotlight.youtubeEmbedId} />
+                            </div>)
+                            :
+                            (<img
+                                src={spotlight.imageURL}
+                                style={{ maxWidth: '800px', maxHeight: '600px' }}
+                                className="rounded mx-auto d-block img-fluid"
+                                alt="random picture probably not relating to the post"
+                            />
+                            )
+                    }
 
-                        <h1 style={{ textAlign: 'center' }}>May's Spotlight</h1>
+                    <div>
 
-                        {homepageResourceLinks.map((link) => {
-                            <HomepageResourceLinksCard key={link.id} link={link} />
-
-                        })}
                     </div>
-                </div>
-            </Col>
+                    <div className="float-right">
 
+                        {' '}
+                    </div>
+
+
+
+
+                </CardBody>
+            </Card>
         </Container>
 
     )
 
 }
 
-export default HelpfulResourceList;
+export default SpotLightListCard;
