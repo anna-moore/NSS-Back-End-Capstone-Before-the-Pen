b33c17a64ccbf06784ff48de6f13fc33602a2b6b
anna-moore <ammoore434@gmail.com>
2021-05-07 09:43:07 -0500

    create type of media provider

---
+++ client/src/providers/TypeOfMediaProvider.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import { UserProfileContext } from './UserProfileProvider';
+
+export const TypeOfMediaContext = createContext();
+
+export function TypeOfMediaProvider(props) {
+    const apiURL = 'api/typeOfMedia';
+
+    const userProfile = sessionStorage.getItem('userProfile');
+    const { getToken } = useContext(UserProfileContext);
+    const [isLoggedIn, setIsLoggedIn] = useState(userProfile != null);
+    const [typeOfMedia, setTypeOfMedia] = useState([]);
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            setCurrentUserId(JSON.parse(userProfile).id);
+        }
+    }, [userProfile]);
+
+    //gather all of the types of media 
+    const getAllTypeofMedia = () => {
+        return getToken()
+            .then((token) =>
+                fetch(`${apiURL}`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                })
+            )
+            .then((res) => res.json())
+            .then(setTypeOfMedia);
+    }
+
+    return (
+        <TypeOfMediaContext.Provider
+            value={{
+                getAllTypeofMedia,
+                setTypeOfMedia,
+                typeOfMedia
+            }}
+        >
+            {props.children}
+        </TypeOfMediaContext.Provider>
+    )
+}
